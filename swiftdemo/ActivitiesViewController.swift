cca060eca56d2f8ae138351c3fed3de60db55dcd
XinMa <maxin5264926@gmail.com>
2015-11-21 02:27:34 +0800

    swiftDemo

---
+++ swiftdemo/ActivitiesViewController.swift
@@ -0,0 +1,9 @@
+//
+//  ActivitiesViewController.swift
+//  swiftdemo
+//
+//  Created by maxin on 15/11/20.
+//  Copyright © 2015年 maxin. All rights reserved.
+//
+
+import Foundation
---
+++ swiftdemo/NewActivitiesViewController.swift
@@ -0,0 +1,9 @@
+//
+//  NewActivitiesViewController.swift
+//  swiftdemo
+//
+//  Created by maxin on 15/11/20.
+//  Copyright © 2015年 maxin. All rights reserved.
+//
+
+import Foundation
